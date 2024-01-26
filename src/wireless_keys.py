487d3dedf89ada4e1919cda2fe6e18ba9e5cc4d2
Evan Wilkerson <evan.wilkerson@meraki.net>
2024-01-26 10:21:17 -0600

    initial commit

---
+++ src/wireless_keys.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+
+__author__ = "Evan Wilkerson"
+__version__ = "0.1.0"
+
+from typing import Type
+import hmac
+import struct
+import math
+from hashlib import _hashlib, pbkdf2_hmac, sha1, sha256, sha384
+
+
+def is_utf8(byte_string: bytes) -> bool:
+    """
+    Check if a byte string is valid UTF-8 encoded.
+
+    Parameters:
+    - byte_string (bytes): The byte string to be checked.
+
+    Returns:
+    - bool: True if the byte string is valid UTF-8 encoded, False otherwise.
+
+    Note:
+    This function attempts to decode the given byte string using UTF-8.
+    If the decoding succeeds, the byte string is considered UTF-8 encoded.
+    If a `UnicodeDecodeError` occurs during decoding, the byte string is not UTF-8 encoded.
+    """
+    try:
+        byte_string.decode('utf-8')
+        return True
+    except UnicodeDecodeError:
+        return False
+
+
+def passphrase_to_psk(passphrase: bytes, ssid: bytes) -> bytes:
+    """
+    Convert a passphrase and SSID to a Pre-Shared Key (PSK) using PBKDF2-HMAC-SHA1.
+
+    Parameters:
+    - passphrase (bytes): ASCII-encoded passphrase.
+    - ssid (bytes): UTF-8-encoded SSID.
+
+    Returns:
+    - bytes: Pre-Shared Key (PSK) generated using PBKDF2-HMAC-SHA1.
+
+    Raises:
+    - ValueError: If the passphrase is not an ASCII encoded byte string,
+                  or if the passphrase length is not between 8 and 63 bytes (characters).
+    - ValueError: If the SSID is not a UTF-8 encoded byte string,
+                  or if the SSID length is more than 32 characters.
+    """
+    if not (type(passphrase) == bytes and passphrase.isascii()):
+        raise ValueError("The passphrase must be an ASCII encoded byte string.")
+    if not 8 <= len(passphrase) <= 63:
+        raise ValueError("Invalid passphrase length. It must be between 8 and 63 bytes (characters).")
+    if not (type(ssid) == bytes and is_utf8(ssid)):
+        raise ValueError("The SSID must be an UTF-8 encoded byte string.")
+    if not len(ssid) <= 32:
+        raise ValueError("Invalid SSID length. It must be 32 characters or less")
+    psk = pbkdf2_hmac('sha1', passphrase, ssid, 4096, 256 // 8)
+    return psk
+
+
+def get_pmkid(pmk: bytes, aa: bytes, spa: bytes, hash_func: Type[_hashlib.HASH]) -> tuple:
+    """
+    Generate PMKID (Pairwise Master Key Identifier) using the given PMK (Pairwise Master Key),
+    Authenticator Address (AA), and Supplicant Address (SPA).
+
+    Parameters:
+    - pmk (bytes): Pairwise Master Key.
+    - aa (bytes): Authenticator Address.
+    - spa (bytes): Supplicant Address.
+    - hash_func: Hash function to use for HMAC (hashlib.sha21, hashlib.sha256 or hashlib.sha384).
+
+    Returns:
+    - bytes: PMKID calculated using HMAC with the specified parameters.
+    """
+    PMKID_LABEL = bytes("PMK Name", "ascii")
+    pmkid = hmac.new(pmk, PMKID_LABEL + aa + spa, hash_func).digest()[:16]
+    return pmkid
+
+
+def getAB(anonce: bytes, snonce: bytes, aa: bytes, spa: bytes) -> tuple:
+    """
+    Generate values A (label) and B (context) for pairwise key expansion.
+
+    Parameters:
+    - anonce (bytes): Supplicant Nonce.
+    - snonce (bytes): Authenticator Nonce.
+    - aa (bytes): Authenticator Address.
+    - spa (bytes): Supplicant Address.
+
+    Returns:
+    - tuple: A tuple containing values A and B.
+      - bytes: Value A ('Pairwise key expansion').
+      - bytes: Value B constructed by concatenating addresses and nonces.
+
+    Note:
+    The inputs (addresses and nonces) are concatenated in a specific order:
+    - min(aa, spa) + max(aa, spa) + min(anonce, snonce) + max(anonce, snonce).
+    """
+    A = b'Pairwise key expansion'
+    B = min(aa, spa) + max(aa, spa) + min(anonce, snonce) + max(anonce, snonce)
+    return A, B
+
+
+def prf(K: bytes, A: bytes, B: bytes, length: int) -> bytes:
+    """
+    Generate pseudo-random key material using SHA-1-based Pseudo-Random Function (PRF).
+
+    Parameters:
+    - K (bytes): Key for the PRF.
+    - A (bytes): Value A.
+    - B (bytes): Value B.
+    - length (int): Length of the desired output in bytes.
+
+    Returns:
+    - bytes: Pseudo-random key material generated using SHA-1-based PRF.
+
+    Note:
+    The input values A and B are concatenated with specific padding and iteration,
+    following the formula A + chr(0) + B + chr(i), where i is the iteration count.
+    """
+    i = 0
+    R = b''
+    while i <= math.ceil((length * 8) / 160):
+        hmacsha1 = hmac.new(K, A + chr(0).encode() + B + chr(i).encode(), sha1)
+        R = R + hmacsha1.digest()
+        i += 1
+    return R[0:length]
+
+
+def kdf(K: bytes, label: bytes, context: bytes, length: int, hash_func: Type[_hashlib.HASH]) -> bytes:
+    """
+    Key Derivation Function (KDF) using HMAC-based Extract-and-Expand Key Derivation Function (HKDF) approach.
+
+    Parameters:
+    - K (bytes): Initial key material.
+    - label (bytes): Label for the specific key derivation operation.
+    - context (bytes): Context information for the key derivation.
+    - length (int): Desired length of the derived key in bytes.
+    - hash_func: Hash function to use for HMAC (hashlib.sha256 or hashlib.sha384).
+
+    Returns:
+    - bytes: Derived key generated using HMAC-based HKDF.
+
+    Note:
+    The HKDF approach involves iteratively applying HMAC with a specific concatenation of values,
+    including the iteration count, label, context, and desired output length.
+    """
+    i = 1
+    result = b''
+    hash_size = hash_func().digest_size * 8
+    while i <= math.ceil((length * 8) / hash_size):
+        hmacsha256 = hmac.new(K, struct.pack('<H', i) + label + context + struct.pack('<H', (length * 8)), hash_func)
+        result = result + hmacsha256.digest()
+        i += 1
+    return result[0:length]
---
+++ tests/test_wireless_keys.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+"""
+python3 -m unittest tests/test_wireless_keys.py
+"""
+
+import unittest
+from src.wireless_keys import *
+import hashlib
+
+
+class TestWirelessKeysFunctions(unittest.TestCase):
+
+
+    def test_passphrase_to_psk(self):
+        ssid = bytes('IEEE', 'ascii')
+        passphrase = bytes('password', 'utf-8')
+
+        psk = passphrase_to_psk(passphrase, ssid)
+
+        self.assertIsInstance(psk, bytes)
+        self.assertEqual(len(psk), 32)
+        self.assertEqual(psk.hex(), 'f42c6fc52df0ebef9ebb4b90b38a5f902e83fe1b135a70e23aed762e9710a12e')
+
+
+    def test_get_pmkids(self):
+        pmk = bytes.fromhex('03304a03fb68e904d47b78429d661f8bc7f373f5de6df640e9b0f3e142a6fe5d')
+        spa = bytes.fromhex('8045dd31c4c8')
+        aa = bytes.fromhex('aa468d005020')
+
+        pmkid_sha1 = get_pmkid(pmk, aa, spa, sha1)
+        self.assertIsInstance(pmkid_sha1, bytes)
+        self.assertEqual(len(pmkid_sha1), 16)
+        self.assertEqual(pmkid_sha1.hex(), '0db738755972d6a09cfadc3e235c9e29')
+
+        pmkid_sha256 = get_pmkid(pmk, aa, spa, sha256)
+        self.assertIsInstance(pmkid_sha256, bytes)
+        self.assertEqual(len(pmkid_sha256), 16)
+        self.assertEqual(pmkid_sha256.hex(), '61eac9970bd64e1ca7820bfe9355b61b')
+
+        pmkid_sha384  = get_pmkid(pmk, aa, spa, sha384)
+        self.assertIsInstance(pmkid_sha384, bytes)
+        self.assertEqual(len(pmkid_sha384), 16)
+        self.assertEqual(pmkid_sha384.hex(), '5c819340218a382d62959ae0d2ecb554')
+
+
+    def test_prf(self):
+        sha1_prf_pmk = bytes.fromhex('8b470d911a0428b393a8396d3e0faa69d994d87f682d293c460f8262a5cbbcb1')
+        sha1_prf_anonce = bytes.fromhex('13474349c681499192f7f6581121b8e60c166925fac52cc29e5d03d5a1e4acf3')
+        sha1_prf_snonce = bytes.fromhex('5d13d1569b435213f9724003655882a4d6d713a894ecd00db13ce46d8f11e960')
+        sha1_prf_spa = bytes.fromhex('2429348bb466')
+        sha1_prf_aa = bytes.fromhex('c29c2ee79b80')
+
+        A, B = getAB(sha1_prf_anonce, sha1_prf_snonce, sha1_prf_aa, sha1_prf_spa)
+        ptk = prf(sha1_prf_pmk, A, B, 48)
+        kck = ptk[0:16]
+        kek = ptk[16:32]
+        tk = ptk[32:48]
+
+        self.assertIsInstance(ptk, bytes)
+        self.assertEqual(len(ptk), 48)
+        self.assertEqual(len(kck), 16)
+        self.assertEqual(len(kek), 16)
+        self.assertEqual(len(tk), 16)
+        self.assertEqual(kck.hex(), '0c18b0dfda634acd02ff19138b6018dd')
+        self.assertEqual(kek.hex(), '4fc9280a6833fdd9dde5c302222c2fc4')
+        self.assertEqual(tk.hex(), 'ac3c0c7e2ba97c718cc5dde645e672ea')
+
+
+    def test_kdf_sha256(self):
+        sha256_kdf_pmk = bytes.fromhex('bf9721e26479f2a412dee0e89a4fef1894c3feceab36d53c6583ce96e3c7b2e5')
+        sha256_kdf_anonce = bytes.fromhex('5417cde51869ebc00200d6ac2e43ea4d774de9aadceb438383beaf696f1601e3')
+        sha256_kdf_snonce = bytes.fromhex('27d0029a0464fdac4c1870740d138c504be995a4e384a5a826ee539a2df5fe7f')
+        sha256_kdf_aa = bytes.fromhex('c29c2ee79b80')
+        sha256_kdf_spa = bytes.fromhex('8045dd31c4c8')
+
+        A, B = getAB(sha256_kdf_anonce, sha256_kdf_snonce, sha256_kdf_aa, sha256_kdf_spa)
+        ptk = kdf(sha256_kdf_pmk, A, B, 48, hashlib.sha256)
+        kck = ptk[0:16]
+        kek = ptk[16:32]
+        tk = ptk[32:48]
+
+        self.assertIsInstance(ptk, bytes)
+        self.assertEqual(len(ptk), 48)
+        self.assertEqual(len(kck), 16)
+        self.assertEqual(len(kek), 16)
+        self.assertEqual(len(tk), 16)
+        self.assertEqual(kck.hex(), '9464f0b1681cb18863edcb0eef087ad8')
+        self.assertEqual(kek.hex(), '206e1a8bfa66ad7f0fd9a63b7b3e31df')
+        self.assertEqual(tk.hex(), 'ec7e2f6ef92b17c9df75007b8a4d437f')
+
+
+    def test_kdf_sha384(self):
+        sha384_kdf_pmk = bytes.fromhex('8bf26829f546c3bb66890da1fc7b0f7e3863f70a41abccd50c735fbc090863fa61639bbb3dd616c3172d068cc0188edd')
+        sha384_kdf_anonce = bytes.fromhex('5c4d0eb6835565c3dc7ee4f8f36193282041a27faf9dc8dbbbc7f16eee627cd0')
+        sha384_kdf_snonce = bytes.fromhex('1ec0830d02bc8a70044ea182a12b88605fcc58495b233092fa8ba119fb94c388')
+        sha384_kdf_aa = bytes.fromhex('c29c2ee79b80')
+        sha384_kdf_spa = bytes.fromhex('8045dd31c4c8')
+
+        A, B = getAB(sha384_kdf_anonce, sha384_kdf_snonce, sha384_kdf_aa, sha384_kdf_spa)
+        ptk = kdf(sha384_kdf_pmk, A, B, 88, hashlib.sha384)
+        kck = ptk[0:24]
+        kek = ptk[24:56]
+        tk = ptk[56:88]
+    
+        self.assertIsInstance(ptk, bytes)
+        self.assertEqual(len(ptk), 88)
+        self.assertEqual(len(kck), 24)
+        self.assertEqual(len(kek), 32)
+        self.assertEqual(len(tk), 32)
+        self.assertEqual(kck.hex(), '2c7aaab3991bd283ba0d5bf830206010eeed382c945c9301')
+        self.assertEqual(kek.hex(), 'b6ea093b60f94f90ae11f26a0f2ba4a2fa10f0ad9a8fd26657ca5b5e1348f0f0')
+        self.assertEqual(tk.hex(), '7091d35e275bf0866fbfcf9be7741c954bc811296f750103873a6e73fa9bd274')
+
+
+if __name__ == '__main__':
+    unittest.main()
